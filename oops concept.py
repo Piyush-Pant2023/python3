31fb1d3a875c53d933ab4a590d22ea68248c386d
Piyush-Pant2023 <piyushpant64@gmail.com>
2024-09-26 23:46:01 +0530

    basics

---
+++ oops concept.py
@@ -0,0 +1,7 @@
+class Piyush():
+    def Add(self,a,b,c):
+        print(a+b+c)
+    def Add(self,a,b):
+        print(a+b)
+o=Piyush()
+o.Add(a:2,b:3)   
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+class Piyush():
+    def Add(self,a,b,c):
+        print(a+b+c)
