2e0fe83c6d754073dca4058f85048a46d8138016
Piyush-Pant2023 <149172283+Piyush-Pant2023@users.noreply.github.com>
2024-12-23 22:56:57 +0530

    Add files via upload

---
+++ New folder (8)/practice.py
@@ -0,0 +1,7 @@
+import random
+print(random.radom())
+print(random.randit(a:1,b:3))
+a<=n<=b
+print(random.uniform(a:1.4,b:2.3))
+a<=n<=b
+print(random.randrange(start:1,stop:13,step:2))
---
+++ New folder (8)/practice2.py
@@ -0,0 +1,5 @@
+def call(p=40,q=20):
+    p=p+q
+    q=p-q
+    print(p,'@',q)
+    return p
