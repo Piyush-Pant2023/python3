0943cee8913bd71a7bab99594579d28249fbedbf
Piyush-Pant2023 <149172283+Piyush-Pant2023@users.noreply.github.com>
2025-05-25 22:09:27 +0530

    Add files via upload

--- loop/practice 2.py
+++ loop/practice 2.py
@@ -6,7 +6,7 @@ upper = 1000
 print("Prime numbers between", lower, "and", upper, "are:")
 
 for num in range(lower, upper + 1):
-   # all prime numbers are greater than 1
+   # all prime numberA\s are greater than 1
    if num > 1:
        for i in range(2, num):
            if (num % i) == 0:
